9778db71831e44e1a4c1bd880464552a463946df
kimcoder <kimcoder@me.com>
2018-08-02 21:08:00 +0900

    17.Dynamic programming add

---
+++ 17.Dynamic programming/17.NumberSolitaire.js
@@ -0,0 +1,17 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const dp = A.map((e, i) => (i === 0) ? A[0] : -Infinity);
+    
+    for (let i=1; i<A.length; i++) {
+        for (let j=Math.max(0, i-6); j<i; j++) {
+            dp[i] = Math.max(dp[i], A[i]+dp[j]);
+            // console.log(`i:${i} j:${j} A[i]:${A[i]} dp[i]:${A[i]+dp[j]}`, dp);
+        }    
+    }
+    console.log(dp);
+    
+    return dp.pop();
+}
---
+++ 17.Dynamic programming/readme.md
@@ -0,0 +1,2 @@
+# 17. Dynamic programming
+- NumberSolitaire : https://app.codility.com/demo/results/training7DBAD4-GEP/
